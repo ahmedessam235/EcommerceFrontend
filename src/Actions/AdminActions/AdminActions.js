5af96b72dd2df7a5164178ae552c263b5fac7dff
ahmedessam235 <ahmedessameltantawy@gmail.com>
2021-06-23 19:08:28 +0200

    adding admin panel and admin conditional rendering for panel

---
+++ src/Actions/AdminActions/AdminActions.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export async function CreateNewAdmin(email, password,token) {
+ const userData = { email: email, password: password };
+  var status = 0;
+  let response = await axios.post("http://localhost:5000/admin", userData, {
+    headers: {
+      'admin-token': token,
+    },
+  });
+  status = response.status;
+  console.log(status);
+  if (status !== 200) {
+    throw new Error("here");
+}else{
+    console.log("we have saved user data");
+    return;
+  }
+}
+
+export default CreateNewAdmin;
--- src/Components/AdminPanel/AdminPanel.js
+++ src/Components/AdminPanel/AdminPanel.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import CreateNewAdmin from '../../Actions/AdminActions/AdminActions';
+import { useHistory } from "react-router-dom";
+
 
 export function AdminPanel(props){
+    let history = useHistory();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     let user = props.user.userDetails;
+
+    async function HandleCreateAdmin(event) {
+        //save to the global context (user data ) for the sign up
+        event.preventDefault();
+        
+        //1- Send email,password,token to a function
+        await CreateNewAdmin(email, password,user.token);
+      }
+      if(user.isAdmin !== true){
+            return (
+                <h1>Unavailable</h1>
+            );
+      }
     return(
         <div>
             <h1>Welcome to admin panel - {user.email}</h1>
+
+            <Container>
+        <form className="form-signin text-center">
+          <label htmlFor="inputEmail " className="sr-only">
+            Email address
+          </label>
+          <Row>
+            <input
+              type="email"
+              id="inputEmail"
+              className="form-control"
+              placeholder="Email address"
+              required=""
+              autoFocus=""
+              onChange={(event)=>{setEmail(event.target.value)}}
+              value={email}
+            />
+          </Row>
+          <label htmlFor="inputPassword" className="sr-only">
+            Password
+          </label>
+          <Row>
+            <input
+              type="password"
+              id="inputPassword"
+              className="form-control "
+              placeholder="Password"
+              required=""
+              value={password}
+              onChange={(event)=>{setPassword(event.target.value)}}
+            />
+          </Row>
+
+          <Row>
+            <button
+              className="btn btn-lg btn-primary btn-block "
+              type="submit"
+              onClick={HandleCreateAdmin}
+              to="/"
+            >
+              Create New Admin
+            </button>
+          </Row>
+          <p className="mt-5 mb-3 text-muted">© 2021-2022</p>
+        </form>
+      </Container>
+
         </div>
-        //1- CRUD Products/Cate/SubCat
-        //2- Create new admins
     );
 }
 
